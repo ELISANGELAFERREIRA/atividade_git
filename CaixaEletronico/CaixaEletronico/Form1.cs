5428aa3ee73cba3e52f508b94e532da3b912e4ab
Elisângela Ferreira <elisange.f@hotmail.com>
2017-12-06 22:48:17 -0200

    commit inicial

---
+++ CaixaEletronico/CaixaEletronico/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaixaEletronico
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // int numeroDaConta= 1;
+
+            /* double saldo = 100.0;
+             double valor = 101.0;
+
+             MessageBox.Show("O saldo atual é " + saldo);
+
+             double saldoAposSaque = saldo - valor;
+
+             MessageBox.Show("O saldo atual é " + saldoAposSaque);
+
+             bool podeSacar = (valor <= saldo) && (valor >= 0) ;
+
+
+             if (podeSacar) {
+
+                 saldo = saldo - valor;
+
+                 MessageBox.Show("Saque realizado com sucesso");
+
+             }
+             else
+             {
+                 MessageBox.Show("Saldo Insufuciente");
+             } */
+
+
+/*            double valorInvestido = 2000.0;
+
+            for (int i = 1; i<=12; i++)
+            {
+                valorInvestido *= 1.01;
+
+           }
+
+            MessageBox.Show("Valor Investido: " + valorInvestido);
+
+            int j= 1;
+
+            valorInvestido = 2000.0;
+
+            while (j <= 12)
+            {
+                valorInvestido *= 1.01;
+                j++;
+            } 
+
+            MessageBox.Show("Valor Investido: " + valorInvestido);
+
+    */
+
+
+
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # atividade_git
 Atividade sobre GIT
+
+Esta é a atividade sobre o GIT
+
+Criar repositório no GitHub
+
+Cloná-lo na máquina
+
+Versionar um projeto no repositório local (file>>new project>> console application)
+
+Ignorar pasta bin
+
+Criar arquivo readme
+
+Submeter o projeto ao repositório remoto
